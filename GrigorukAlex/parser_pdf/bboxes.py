248c821f577fbd6293c4e04e1ed9eb0c212bc28a
grigorukalex <grigorukalex@gmail.com>
2023-12-07 02:34:21 +0700

    -

--- GrigorukAlex/parser_pdf/bboxes.py
+++ GrigorukAlex/parser_pdf/bboxes.py
@@ -109,7 +109,7 @@ def get_bboxes_tables_page(pdf_path, skip_num_tables, padding):
         # Находим координаты таблиц на текущей странице
         tables = page.find_tables()
 
-        # Удаляем первые 4 таблице у первого листа
+        # Удаляем первые 4 таблицы у первого листа
         if page_num == 0 and len(tables) > 3:
             tables = tables[skip_num_tables:]
         print('Кол-во таблиц на странице ', page_num + 1, ' : ', len(tables), ' шт')
@@ -206,6 +206,31 @@ def draw_bboxes(path_imgs, bboxes_pages):
         # Сохраняем изображение
         cv2.imwrite(path_img_page, img_page)
         # cv2_show("Image", img_page)
+
+
+# Вырезаем bbox из изображения
+def cut_bbox_from_img(path_img, bbox):
+    """
+    :param path_img: путь к изображению
+    :param bbox: bbox {'left':0.1, 'right':0.8, 'top':0.2, 'bottom':0.5) для изображения
+        в относительных значениях (от 0 до 1)
+    :return: вырезанное изображение
+    """
+    # Открываем изображение текущей страницы PDF
+    img = cv2.imread(path_img)
+    # Определяем размеры изображения
+    width, height = img.shape[1], img.shape[0]
+
+    # Переводим относительные (0-1) в абсолютные координаты
+    left = int(bbox['left'] * width)
+    top = int(bbox['top'] * height)
+    right = int(bbox['right'] * width)
+    bottom = int(bbox['bottom'] * height)
+
+    # Вырезаем bbox из изображения
+    img_bbox = img[top:bottom, left:right]
+
+    return img_bbox
 
 
 def parse(pdf_path, skip_num_tables, padding, dpi=100, flag_draw_bboxes=True):
